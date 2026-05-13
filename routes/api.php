bc28c3ed354554d3b1b2c574fc6612e9e767e2f3
DomenicoFoglia <foglia.dmnc@gmail.com>
2026-05-13 20:09:20 +0200

    Feat: Aggiunta rotta per utente

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,9 @@ use App\Http\Controllers\InvitationController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TicketController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/register', [AuthController::class, 'register']);
@@ -18,10 +21,14 @@ Route::post('/auth/invite/{token}', [AuthController::class, 'registerByInvite'])
 
 Route::middleware('auth:sanctum')->group(function () {
 
+    Route::get('/user', function (Request $request) {
+        return User::with('company')->find(Auth::id());
+    });
     Route::post('/auth/logout', [AuthController::class, 'logout']);
     Route::get('/faqs', [FaqController::class, 'index']);
     Route::get('/faqs/{id}', [FaqController::class, 'show']);
     Route::put('/user/theme', [ProfileController::class, 'updateTheme']);
+    
 
     Route::middleware('is.admin')->group(function () {
         Route::get('/admin/invitations', [InvitationController::class, 'index']);
