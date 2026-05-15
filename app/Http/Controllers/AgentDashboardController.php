7dc040d8d367389c561ee73fb357a70941f4bba4
DomenicoFoglia <foglia.dmnc@gmail.com>
2026-05-15 18:02:25 +0200

    Feat: Aggiunto numero di ticket risolti da tecnico nella settimana

--- app/Http/Controllers/AgentDashboardController.php
+++ app/Http/Controllers/AgentDashboardController.php
@@ -15,9 +15,33 @@ class AgentDashboardController extends Controller
                                     ->where('assignee_id', $user->id)
                                     ->whereToday('closed_at')
                                     ->count();
+
+        $closedTicketsWeek = Ticket::where('company_id', $user->company_id)
+                                    ->where('assignee_id', $user->id)
+                                    ->where('closed_at', '>', now()->subDays(7))
+                                    ->selectRaw('DATE(closed_at) as date, COUNT(*) as count')
+                                    ->groupBy('date')
+                                    ->orderBy('date')
+                                    ->get();
         
+        $closedTicketsWeekArray = [];
+
+        for( $i = 6; $i >= 0; --$i){
+            
+            $day = now()->subDays($i)->format('Y-m-d');
+
+            $found = $closedTicketsWeek->firstWhere('date', $day);
+            if($found){
+                $closedTicketsWeekArray[] = $found->count;
+            }else{
+                $closedTicketsWeekArray[] = 0;
+            }
+        }
+
+
         return response()->json([
-            'closedTicketsToday' => $closedTicketsToday
+            'closedTicketsToday' => $closedTicketsToday,
+            'closedTicketsWeek' => $closedTicketsWeekArray
         ]);
 
     }
