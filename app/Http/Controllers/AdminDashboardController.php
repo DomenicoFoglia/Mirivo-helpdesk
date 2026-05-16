5e36648fdfd36161c6b2472f6b9b299e60af0a85
DomenicoFoglia <foglia.dmnc@gmail.com>
2026-05-16 13:08:05 +0200

    Feat: Aggiunto metodo details alla dashboard admin(ticket che richiedo attenzione, tecnici, inviti in attesa e Faq recenti)

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Faq;
+use App\Models\Invitation;
 use App\Models\Ticket;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -27,6 +30,41 @@ class AdminDashboardController extends Controller
             'workingTickets' => $workingTickets,
             'closedTicketsToday' => $closedTicketsToday,
             'withoutAnswerTickets' => $withoutAnswerTickets
+        ]);
+    }
+
+    public function details(){
+        $user = Auth::user();
+
+        $attentionTickets = Ticket::select(['id', 'title', 'priority', 'updated_at'])
+                                    ->where('company_id', $user->company_id)
+                                    ->where('status', '!=', 'closed')
+                                    ->where('updated_at', '<', now()->subHours(24))
+                                    ->get();
+        
+        $agents = User::select(['id', 'name', 'level'])
+                        ->where('company_id', $user->company_id)->where('role', 'agent')
+                        ->withCount(['assigneeTickets' => function ($query) {
+                            $query->where('status', '!=', 'closed');
+                        }])->get();
+
+        $pendingInvitations = Invitation::select(['email', 'role', 'created_at'])
+                                        ->where('company_id',  $user->company_id)
+                                        ->whereNull('accepted_at')
+                                        ->where('expires_at', '>', now())
+                                        ->get();
+
+        $recentFaqs = Faq::select(['id', 'question', 'created_at'])
+                    ->where('company_id', $user->company_id)
+                    ->orderBy('created_at', 'desc')
+                    ->limit(5)
+                    ->get();
+
+        return response()->json([
+            'attentionTickets' => $attentionTickets,
+            'agents' => $agents,
+            'pendingInvitations' => $pendingInvitations,
+            'recentFaqs' => $recentFaqs
         ]);
     }
 }
--- app/Http/Controllers/AgentDashboardController.php
+++ app/Http/Controllers/AgentDashboardController.php
@@ -45,4 +45,5 @@ class AgentDashboardController extends Controller
         ]);
 
     }
+
 }
