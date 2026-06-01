07081b0bf5dceff1a1aedf0efa4ae3792d44bd6c
DomenicoFoglia <foglia.dmnc@gmail.com>
2026-06-01 13:08:10 +0200

    Fix: CategoryController index da paginate a get

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -15,7 +15,7 @@ class CategoryController extends Controller
     {
         $user = Auth::user();
 
-        $categories = Category::where('company_id', $user->company_id)->paginate(15);
+        $categories = Category::where('company_id', $user->company_id)->get();
 
         return $categories;
     }
